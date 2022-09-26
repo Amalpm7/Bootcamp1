38f391bb635acd5404db175e971c30efef7405e0
Amalpm7 <amal.p@nestgroup.net>
2022-09-26 23:50:04 +0530

    2nd commit

--- src/com/Nest1/development/development.java
+++ src/com/Nest1/development/development.java
@@ -2,6 +2,6 @@ package com.Nest1.development;
 
 public class development {
     public static void main(String[] args) {
-        System.out.println("Hi");
+        System.out.println("Hey bud");
     }
 }
